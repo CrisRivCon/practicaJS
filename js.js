64ef90724987b3d9b06685a7a30e2c4fce2fd4ce
Julio <soporte1@xerintel.es>
2022-03-23 14:40:43 +0100

    cambio titulo

--- js.js
+++ js.js
@@ -29,7 +29,7 @@ boton.addEventListener("click", function(event){
     crearBoton.setAttribute("data-target", "#ventana");
     crearBoton.innerHTML = "Modificar";
     document.querySelector(("#f"+numeroFila)+":last-child").append(crearBoton);
-    crearBoton.addEventListener("click", function(event){
+    /* crearBoton.addEventListener("click", function(event){ 
         for(let a = 1;a<5;a++){
             let texto = document.getElementById("c"+numeroFila+a);
             let campM = document.getElementById("campM"+a);
@@ -44,7 +44,7 @@ boton.addEventListener("click", function(event){
                 texto.innerHTML = campM.value;
             };
         });
-    });
+    }); */
     };
  });
 
@@ -54,6 +54,31 @@ boton.addEventListener("click", function(event){
 // Uno de los metodos que trae los modales son para modificar el funcionamiento cuando los abres o cierras
 $('#ventana').on('show.bs.modal', function (event) {
     console.log(event.relatedTarget.id)  //accedes a la propiedad id
+    let fila = event.relatedTarget.id; //m1
+     document.getElementById(fila+'_1').value  // asi traemos el nombre de la fila 1
+     document.getElementById(fila+'_2').value //apellido
+     document.getElementById(fila+'_3').value // genero ,almacenas todos los valores e insertas
 
+     document.getElementById('campM1').value =  document.getElementById(fila+'_1').value;
+     //No necesitas el codigo de arriba
   // Leer el elemento (event) que dispara este evento, opbtener de el el id de la fila que has clicado
   }) 
+
+  //No hace falta que al cerrar se limpien porque se vuelve a reesscribir cuando vuelvas a abrir el modal
+  // Te miro git rapido
+  //vale, te digo los comandos basicos para subirlo y el lunes te explico mejor
+
+  // git init => inicializas un repositorio local
+
+  // git status => compruebas el estado de los ficheros
+  // Rojo= no añadidos al staged ( zona de arcchivos proeparados para subir a repo remoto)
+  // Verde = archivos con modificaciones confirmadas, preparados para subirlo a repo remoto
+  // git add . (. para añadir todos los ficheros al stage area)
+
+  //Mo eolvide de commit (git commit -m "descripcion": confirmar cambios)
+  // No hay repo remoto, lo aádimos agregar repo remoto: git remote add <nombre> <url>
+
+  //subir cambios a repo remoto git push // git push --set-upstream origin master/main (depende)
+  //--set-upstream  solo la primera vez, apunta hacia la rama principal remota
+
+ 
