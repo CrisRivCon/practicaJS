f910fc765b1b2ed6aaebae497ce6f8347ae66997
Julio <soporte1@xerintel.es>
2022-03-24 14:45:35 +0100

    añadido boton de eliminar con confirmacion, eliminado ID, cambio de titulo de edit

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var saveBtn = document.getElementById("boton_guardar");
 var numFila = 0;
-var id = 1;
+//var id = 1;
 //var numeroBoton = 0; /* nº de fila y de boton pueden ser iguales, te sobra una variable => numelemento */
 //boton ?? guardar o actualizar ? las variables deben ser descriptivas
 saveBtn.addEventListener("click", function(event){  //Boton para guardar en la tabla la informacion de los input
@@ -9,8 +9,9 @@ saveBtn.addEventListener("click", function(event){  //Boton para guardar en la t
     if(document.getElementById("form_input_1").value !== ""){
     let fila = document.createElement("tr");
     fila.setAttribute("id", "elem_tabla_fila"+numFila);
+    fila.setAttribute("data-index-number", 'fila'+numFila)
 
-    for(let i = 0; i<5; i++){
+    for(let i = 1; i<5; i++){
         let campo = document.createElement("td");
         let texto = document.getElementById("form_input_"+i).value;
         campo.setAttribute("id", "elem_tabla_"+numFila+"_"+i);
@@ -20,17 +21,35 @@ saveBtn.addEventListener("click", function(event){  //Boton para guardar en la t
 
     document.querySelector("tbody").append(fila);
     //EditBtn por ej
-    let editBtn = document.createElement("button");
+    let editBtn = document.createElement("button");//Crear boton modificar
     editBtn.setAttribute("id", "elem_tabla_"+numFila);
     editBtn.setAttribute("class", "btn btn-secondary");
     editBtn.setAttribute("type", "button");
     editBtn.setAttribute("data-toggle", "modal");
     editBtn.setAttribute("data-target", "#modal_edit");
+    editBtn.setAttribute("data-index-number", numFila);//----------------------
     editBtn.innerHTML = "Editar";
     document.querySelector(("#elem_tabla_fila"+numFila)+":last-child").append(editBtn);
+
+    let delBtn = document.createElement("button"); //Crear boton eliminar
+    delBtn.setAttribute("id", "elem_tabla_del_"+numFila);
+    delBtn.setAttribute("class", "btn btn-danger");
+    delBtn.setAttribute("type", "button");
+    delBtn.setAttribute("data-toggle", "modal");
+    delBtn.setAttribute("data-target", "#modal_eliminar");
+    delBtn.setAttribute("data-index-number", numFila);//------------------------------
+    delBtn.innerHTML = "Eliminar";
+    document.querySelector(("#elem_tabla_fila"+numFila)+":last-child").append(delBtn);
+
+    //delBtn.addEventListener('click', function(event){
+    //    let delFila = document.getElementById('elem_tabla_fila_' + numFila);
+    //})
+
     document.getElementById('form_input').reset();
-    id++;
-    document.getElementById('form_input_0').setAttribute('value', id)
+
+    //id++;
+    //document.getElementById('form_input_0').setAttribute('value', id);
+    
     /* editBtn.addEventListener("click", function(event){ 
         for(let a = 1;a<5;a++){
             let texto = document.getElementById("c"+numeroFila+a);
@@ -56,25 +75,43 @@ saveBtn.addEventListener("click", function(event){  //Boton para guardar en la t
 $('#modal_edit').on('show.bs.modal', function (event) {
     //console.log(event.relatedTarget.id)  //accedes a la propiedad id
     let numeFila = event.relatedTarget.id; //m1--elem_tabla_1
-    for(let i=0;i<5;i++){
+    //let indexNum = event.dataset.indexNumber//-------------------------
+    let regex = /(\d+)/g;
+    let indexNum = numeFila.match(regex)
+
+    for(let i=1;i<5;i++){
         let textoTabla = document.getElementById(numeFila+'_'+i);  // asi traemos el nombre de la fila 1
         //let apellidos = document.getElementById(fila+'_2').innerHTML //apellido
         //let email = document.getElementById(fila+'_3').innerHTML // genero ,almacenas todos los valores e insertas
         //let genero = document.getElementById(fila+'_4').innerHTML
         let campoEdit = document.getElementById('edit_input_'+i);
+        campoEdit.dataset.indexNumber = indexNum;
         campoEdit.value = textoTabla.innerHTML;
     }
   });
   var guardarCambios = document.getElementById('guardar_cambios');
   guardarCambios.addEventListener('click', function(event){
-    let numeID = document.getElementById('edit_input_0').value
-    for(let i=0;i<5;i++){
+    let numeID = document.getElementById('edit_input_1').dataset.indexNumber;
+    for(let i=1;i<5;i++){
         let campoEdit = document.getElementById('edit_input_'+i);
-        let textoTabla = document.getElementById('elem_tabla_'+numeID+'_'+i);  // asi traemos el nombre de la fila 1
+        let textoTabla = document.getElementById('elem_tabla_'+numeID+'_'+i);
         textoTabla.innerHTML = campoEdit.value;
     };
   });
-
+  $('#modal_eliminar').on('show.bs.modal', function (event) {
+    let numeFila = event.relatedTarget.id;
+    let regex = /(\d+)/g;
+    let indexNum = numeFila.match(regex);
+    let eliminarConfirma = document.getElementById('confirmar_eliminar');
+    eliminarConfirma.dataset.indexNumber = indexNum;
+  });
+  var eliminarFila = document.getElementById('confirmar_eliminar');
+  eliminarFila.addEventListener('click', function(event){
+    let numeFila = eliminarFila.dataset.indexNumber;
+    let filaEliminada = document.getElementById('elem_tabla_fila'+numeFila);
+    let padreFila = filaEliminada.parentNode;
+    padreFila.removeChild(filaEliminada);
+  });
 
 
 //-----------------------------------------------------------------------------------
