8c1ed0b88adc98fa52b7d0198dacbd67a3c745b3
Cris Riv <cristinapruebas14@gmail.com>
2022-03-30 10:29:40 +0200

    contador y ver propiedades

--- clases/js/practicaclases.js
+++ clases/js/practicaclases.js
@@ -8,6 +8,11 @@ class Persona{
         this.altura = altura;
         this.peso = peso;
         //this.imc = imc;
+        if(Persona.contador) {
+            Persona.contador++;
+          } else {
+            Persona.contador = 1;
+          }
     }
     get IMC(){
         return this.peso / this.altura;
@@ -24,33 +29,36 @@ class Persona{
     comer(){
         console.log("Se va a comer sushi");
     }
+    fichaCompleta(){ //Metodo para ver todas las propiedades?
+        console.log('Nombre: '+ this.nombre + ', Apellidos: ' + this.apellidos +', Edad: ' + this.edad + ', Altura: ' + this.altura + ', Peso: ' + this.peso + ', IMC: ' + this.IMC);
+    }
 
 }
 class Profesor extends Persona{
-    constructor(numProfesor, especialidad, añosDeCarrera){
-        super();
+    constructor(nombre, apellidos, edad, altura, peso, numProfesor, especialidad, añosDeCarrera){
+        super(nombre, apellidos, edad, altura, peso);
         this.numProfesor = numProfesor;
         this.especialidad = especialidad;
         this.añosDeCarrera = añosDeCarrera;
     };
     corregir(){
-        console.log("El profesor "+this.nombre)+" está corrigiendo.";
+        console.log("El profesor "+this.nombre+" está corrigiendo.");
     }
     darClase(){
-        console.log("El profesor "+this.nombre)+" está dando clase.";
+        console.log("El profesor "+this.nombre+" está dando clase.");
     }
 
 }
 class Alumno extends Persona{
-    constructor(idAlumno, notaMedia){
-        super();
+    constructor(nombre, apellidos, edad, altura, peso, idAlumno, notaMedia){
+        super(nombre, apellidos, edad, altura, peso);
         this.idAlumno = idAlumno;
         this.notaMedia = notaMedia;
     };
     estudiar(){
         console.log("El alumno " + this.nombre +" está estudiando.");
     }
-    estudiar(){
+    dormir(){
         console.log("El alumno " + this.nombre +" está durmiendo.");
     }
 
@@ -62,7 +70,12 @@ console.log('El IMC de ' + profesoraAna.nombre + ' es de ' + profesoraAna.IMC);
 profesoraAna.nuevoPeso = 70;
 console.log('El peso de ' + profesoraAna.nombre + ' es de ' + profesoraAna.peso);
 console.log('El IMC de ' + profesoraAna.nombre + ' es de ' + profesoraAna.IMC);
+profesoraAna.fichaCompleta();
+console.log('Se han instanciado '+ Persona.contador + ' personas.');
 
+const alumnoJavi = new Alumno('Javi','Lopez', '15', '1.40', '50', '123', '9')
+alumnoJavi.fichaCompleta();
+console.log('Se han instanciado '+ Persona.contador + ' personas.');
 console.log('--------------------------Objeto literal----------------------------');
 
 //-----------------------------Objeto literal------------------------------
@@ -70,7 +83,7 @@ var alumnoJuan={
     nombre:'Juan',
     notaExamen1: 7,
     notaExamen2: 5,
-    notaExamen3:  8,
+    notaExamen3: 8,
     notaMedia:function(puntosPositivos = 0){
         return ((this.notaExamen1 + this.notaExamen2 + this.notaExamen3)/3)+ puntosPositivos;
     },
