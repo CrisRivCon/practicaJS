7b089d42c3abd54c9f1ee2321d345859f8053b08
Cris Riv <cristinapruebas14@gmail.com>
2022-03-30 08:45:54 +0200

    getter y setter

--- clases/js/practicaclases.js
+++ clases/js/practicaclases.js
@@ -1,30 +1,35 @@
+console.log('--------------------------Clases----------------------------')
+
 class Persona{
-    constructor(nombre, apellidos, edad, altura, peso, imc){
+    constructor(nombre, apellidos, edad, altura, peso){
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
         this.altura = altura;
         this.peso = peso;
-        this.imc = imc;
+        //this.imc = imc;
     }
-    //get nombre(){
-    //    
-    //}
+    get IMC(){
+        return this.peso / this.altura;
+    }
+    set nuevoPeso(valor){
+        this.peso = valor;
+    }
     hablar(){
         console.log(this.nombre+" está hablando");
     }
     andar(){
         console.log(this.nombre+" anda");
-    };
+    }
     comer(){
         console.log("Se va a comer sushi");
-    };
+    }
 
 }
 class Profesor extends Persona{
-    constructor(nProfesor, especialidad, añosDeCarrera){
+    constructor(numProfesor, especialidad, añosDeCarrera){
         super();
-        this.nProfesor = nProfesor;
+        this.numProfesor = numProfesor;
         this.especialidad = especialidad;
         this.añosDeCarrera = añosDeCarrera;
     };
@@ -40,25 +45,34 @@ class Alumno extends Persona{
     constructor(idAlumno, notaMedia){
         super();
         this.idAlumno = idAlumno;
-        this.nonotaMedia = notaMedia;
+        this.notaMedia = notaMedia;
     };
     estudiar(){
-        console.log("El alumno "+this.nombre)+" está estudiando.";
+        console.log("El alumno " + this.nombre +" está estudiando.");
     }
     estudiar(){
-        console.log("El alumno "+this.nombre)+" está durmiendo.";
+        console.log("El alumno " + this.nombre +" está durmiendo.");
     }
 
 }
+
+const profesoraAna = new Persona('Ana', 'García', 29, 1.60, 60);
+console.log('El peso de ' + profesoraAna.nombre + ' es de ' + profesoraAna.peso);
+console.log('El IMC de ' + profesoraAna.nombre + ' es de ' + profesoraAna.IMC);
+profesoraAna.nuevoPeso = 70;
+console.log('El peso de ' + profesoraAna.nombre + ' es de ' + profesoraAna.peso);
+console.log('El IMC de ' + profesoraAna.nombre + ' es de ' + profesoraAna.IMC);
+
+console.log('--------------------------Objeto literal----------------------------');
 
 //-----------------------------Objeto literal------------------------------
 var alumnoJuan={
     nombre:'Juan',
     notaExamen1: 7,
-    notaExamen2:5,
-    notaExamen3:8,
+    notaExamen2: 5,
+    notaExamen3:  8,
     notaMedia:function(puntosPositivos = 0){
-        return ((this.notaExamen1 + this.notaExamen2 + this.notaExamen3)/3)+puntosPositivos;
+        return ((this.notaExamen1 + this.notaExamen2 + this.notaExamen3)/3)+ puntosPositivos;
     },
     notasTrimestrales:{ //anidacion de objetos
         1: {
@@ -74,7 +88,6 @@ var alumnoJuan={
             nota: 3
         }
     }
-
 }
 
 alumnoJuan.resultadosTercerTrimestre = function(){ //Extension
@@ -88,4 +101,5 @@ alumnoJuan.resultadosTercerTrimestre = function(){ //Extension
 console.log("La nota media de Juan es " + alumnoJuan.notaMedia());
 console.log("La nota media de Juan sumandole los positivos es " + alumnoJuan.notaMedia(1));
 console.log("La nota del segundo trimestre de "+ alumnoJuan.nombre+" es de "+ alumnoJuan.notasTrimestrales[2].nota);
-console.log(alumnoJuan.resultadosTercerTrimestre()); //Por que undefined despues de esto?
+console.log(alumnoJuan.resultadosTercerTrimestre());
+console.log('No entiendo por que undefined');
