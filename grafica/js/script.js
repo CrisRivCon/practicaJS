ab5fb95d953d7f6a09f5be4f2bc03395adcdc7b5
Cristina Riv <cristinapruebas14@gmail.com>
2022-04-06 09:28:25 +0200

    añadir media

--- grafica/js/script.js
+++ grafica/js/script.js
@@ -1,27 +1,48 @@
 const ctx = document.getElementById('myChart');
-var chartBtn = document.getElementById('crearGrafica');
-var inputNum = document.getElementById('inputNumber');
+let chartBtn = document.getElementById('crearGrafica');
+let inputNum = document.getElementById('inputNumber');
+let mediaCard = document.getElementById('media_card');
+let mediaCardBody = document.getElementById('media_card_body');
+let arrayLength = Number(inputNum.value);
+let dataNum=[]; 
+let labels=[];
+let randomNum;
 
-function random (min, max){ //funcion para crear los numeros aleatorios.
+const random =  (min, max) => { //funcion para crear los numeros aleatorios.
     return Math.floor(Math.random() * (max - min) + min);
 };
+const media=()=>{
+    let suma = 0;
+    let numValores = arrayLength;
+    for (const key in labels) {
+        suma += dataNum[key];
+    };
+    return (suma/numValores);
+ };
 //funcion para crear array para labels del rango del numero de valores.
-const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
-        
+//const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+//labels = range(1, arrayLength, 1);
+//for (let n = 0; n<=arrayLength;n++){ //crear array de num aleatorios.
+    //    let randomNum = random(1, 100);
+    //    dataNum.push(randomNum);
+//};
 
 chartBtn.addEventListener('click', event =>{
-    let arrayLength = Number(inputNum.value);
+
     if(arrayLength<=20&&arrayLength>=5){
         if (window.data) { //limpiar la grafica.
+            dataNum = [];
+            labels=[];
             window.data.clear();
             window.data.destroy();
+        }; 
+
+        for (let n = 0; n<=arrayLength;n++){
+            labels.push(n)
         };
-        let dataNum=[];  
 
-        let labels = range(1, arrayLength, 1);
-
-        for (let n = 0; n<=arrayLength;n++){ //crear array de num aleatorios.
-            let randomNum = random(1, 100);
+        for (const key in labels){ //asignar valores aleatorios
+            randomNum = random(1, 100);
             dataNum.push(randomNum);
         };
 
@@ -38,6 +59,9 @@ chartBtn.addEventListener('click', event =>{
                 }]
             },
         });
+        mediaCardBody.innerHTML=`La media de valores es: ${media()}`;
+        mediaCard.classList.remove('d-none');
+        mediaCard.classList.add('d-block');
     }else{ //alerta de numero fuera de rango.
         let invalidNum = document.getElementById('invalid_num_text');
         invalidNum.classList.remove('d-none');
