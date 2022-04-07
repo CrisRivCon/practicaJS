3da8588c73b43bad7170cb1ec9f76374b109a421
Cristina Riv <cristinapruebas14@gmail.com>
2022-04-07 11:54:41 +0200

    Formulario con php

--- practicaPHP/formulario/formulario.php
+++ practicaPHP/formulario/formulario.php
@@ -1,4 +1,9 @@
 <?php
-    echo "Hola que tal";
-    echo "Hola ".$_GET['name'];
+    if(isset($_POST['submit'])&& $_SERVER["REQUEST_METHOD"]=="POST"){
+        echo "Hola ".$_POST['name']. "</br>";
+        echo " con apellidos ".$_POST['last_name']. "</br>";
+        echo " email ".$_POST['email']. "</br>";
+        echo " has seleccionado ".$_POST['exampleRadios']. "</br>";
+    }
+
 ?>
